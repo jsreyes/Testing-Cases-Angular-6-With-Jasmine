a6d69859f902ae8c5a8dcf0831aa1b8adf25d874
Javier Reyes <jreyes@portalfinance.co>
2019-04-04 19:03:34 -0500

    Introduce to testing components

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,37 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  // Se define la variable
+  let component: VoteComponent;
+
+  // Método que se lanza antes de cada prueba
+  beforeEach(() => {
+    // Se inicializa el componente
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  // Método que se lanza despues de cada prueba (Sirve para hacer limpieza)
+  afterEach(() => {});
+
+  // Método que se lanza antes de todas las pruebas
+  beforeAll(() => {});
+
+  // Método que se lanza despues de todas las pruebas
+  afterAll(() => {});
+
+  it('should increment totalVotes when upvoted', () => {
+    // Llama la función de component (Act: Acto invoce método o función)
+    component.upVote();
+
+    // Función que evalua el resultado que se espera (Assert: Afirmar)
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downvoted', () => {
+    // Llama la función de component (Act: Acto invoce método o función)
+    component.downVote();
+
+    // Función que evalua el resultado que se espera (Assert: Afirmar)
+    expect(component.totalVotes).toBe(-1);
   });
 });
