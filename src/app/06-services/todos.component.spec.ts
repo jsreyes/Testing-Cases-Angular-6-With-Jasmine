e1bb5d343f0511d1307e70d49662a16d1c57a22a
Javier Reyes <jreyes@portalfinance.co>
2019-04-05 16:01:48 -0500

    Testing service, that add and response error

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,9 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from 'rxjs';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
+
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -25,5 +28,36 @@ describe('TodosComponent', () => {
 
     // expect(component.todos.length).toBeGreaterThan(0); Llamada general
     expect(component.todos.length).toBe(3); // Llamada especifica (Assert)
+  });
+
+  it('should call the server to save the changes when a new todo item is added', () => {
+
+    const spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo retruned from the server', () => {
+    const todo = { id: 1 };
+    // Se puede reemplazar el call fake por return value
+    const spy = spyOn(service, 'add').and.returnValue(Observable.from([ todo ]));
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should set the message property if server returns an error when adding a new todo ', () => {
+    const error = 'error from the server';
+    // Se puede reemplazar el call fake por return value
+    const spy = spyOn(service, 'add').and.returnValue(Observable.throw(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
   });
 });
